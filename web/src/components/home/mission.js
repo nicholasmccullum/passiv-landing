5fee0b6f45deee2576ab4c2c7e7fb0a989f2ae5d
Nicholas McCullum <nicholasmccullum@gmail.com>
2020-03-04 10:20:10 -0400

    changed mission text

--- web/src/components/home/mission.js
+++ web/src/components/home/mission.js
@@ -14,7 +14,7 @@ function Mission ({  }) {
       </div>
       <div className={styles.copyContainer}>
         <h2><span>The</span> Mission</h2>
-        <p>At Passiv we believe that you should not have to work hard at making your investments work for you. We make it easy to maintain your portfolio’s target allocation by taking care of the calculations and letting you synchronize your holdings with one click. This way you stay balanced and spend less time with spreadsheets.</p>
+        <p>At Passiv we believe that you should not have to work hard at making your investments work for you. We send you email notifications whenever cash hits your account, whether through dividends or contributions. We also let you set target allocations to rebalance your portfolio with one click.</p>
         <a href="/about/" className={styles.btn3}><span>About Passiv</span></a>
       </div>
     </Container>
